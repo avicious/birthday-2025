ccbdebbc57e465e9ca1aac73f443f129f6cd7ea5
Shubham Kumar <shubham.kr2951@gmail.com>
2025-03-29 09:27:46 +0530

    built player component

---
+++ app/components/player.js
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+const Player = () => {
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [image, setImage] = useState("play");
+
+  const audioRef = useRef(null);
+
+  const onClickHandler = () => {
+    if (image === "play") {
+      setImage("pause");
+    } else {
+      setImage("play");
+    }
+
+    setIsAudioPlaying((prev) => !prev);
+  };
+
+  const onEndHandler = () => {
+    setImage("play");
+    setIsAudioPlaying(false);
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+    }
+  };
+
+  useEffect(() => {
+    if (isAudioPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isAudioPlaying]);
+
+  return (
+    <>
+      <div className="container">
+        <img className="square" src="/birthday.webp" />
+        <div className="media">
+          <img onClick={onClickHandler} src={`/${image}.png`} />
+          <audio ref={audioRef} onEnded={onEndHandler} src="/audio.mp3" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Player;
--- app/page.js
+++ app/page.js
@@ -1,50 +1,7 @@
-"use client";
-
-import { useEffect, useRef, useState } from "react";
+import Player from "./components/player";
 
 const page = () => {
-  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
-  const [image, setImage] = useState("play");
-
-  const audioRef = useRef(null);
-
-  const onClickHandler = () => {
-    if (image === "play") {
-      setImage("pause");
-    } else {
-      setImage("play");
-    }
-
-    setIsAudioPlaying((prev) => !prev);
-  };
-
-  const onEndHandler = () => {
-    setImage("play");
-    setIsAudioPlaying(false);
-    if (audioRef.current) {
-      audioRef.current.currentTime = 0;
-    }
-  };
-
-  useEffect(() => {
-    if (isAudioPlaying) {
-      audioRef.current.play();
-    } else {
-      audioRef.current.pause();
-    }
-  }, [isAudioPlaying]);
-
-  return (
-    <>
-      <div className="container">
-        <img className="square" src="/birthday.webp" />
-        <div className="media">
-          <img onClick={onClickHandler} src={`/${image}.png`} />
-          <audio ref={audioRef} onEnded={onEndHandler} src="/audio.mp3" />
-        </div>
-      </div>
-    </>
-  );
+  return <Player />;
 };
 
 export default page;
